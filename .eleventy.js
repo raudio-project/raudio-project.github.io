bdff5f57f589a269019640021e5d78f6043245d2
Simon Rodriguez <esimonrodrig@gmail.com>
2022-02-09 22:20:39 -0500

    some additional sass config

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,12 @@
 module.exports = (eleventyConfig) => {
 
+    // Tells BrowserSync where the CSS files are
     eleventyConfig.setBrowserSyncConfig({
         files: './dist/css/**/*.css'
     })
+
+    // Tells 11ty to simply pass these files through into the final build
+    eleventyConfig.addPassthroughCopy('./src/assets/')
 
     return {
         dir: {
