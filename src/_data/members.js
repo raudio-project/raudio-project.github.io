35f27fff8d0faf2c79abd21c5a56c1f3ef595b1a
Simon Rodriguez <esimonrodrig@gmail.com>
2022-04-07 22:42:18 -0400

    complete about page

--- src/_data/members.js
+++ src/_data/members.js
@@ -7,29 +7,34 @@ module.exports = [
     {
         name: "Simon Rodriguez",
         title: "Website Maintainer",
-        img: { src: "https://images.pexels.com/photos/7449049/pexels-photo-7449049.jpeg", alt: "Image of otter"},
+        img: { src: "/assets/headshot-simon.png", alt: "Headshot of Simon"},
         links: [
-            {icon: ICONS('github'), href: "https://google.com"}
+            {icon: ICONS('github'), href: "https://github.com/simonrodrig"},
+            {icon: ICONS('globe'), href: "https://rodrig.dev"},
         ]
     },
     {
         name: "Thomas Faughnan",
         title: "Server Developer",
-        img: { src: "https://avatars.githubusercontent.com/u/47096018?v=4", alt: "Image of Tom"},
+        img: { src: "https://avatars.githubusercontent.com/u/47096018?v=4", alt: "Headshot of Tom"},
         links: [
-            {icon: ICONS('x'), href: "https://wikipedia.com"},
+            {icon: ICONS('github'), href: "https://github.com/tfaughnan"},
         ]
     },
     {
         name: "Paul Gierl",
-        title: "Server Developer",
-        img: { src: "https://avatars.githubusercontent.com/u/66021781?v=4", alt: "Image of Paul"},
-        links: []
+        title: "Server/Bot Developer",
+        img: { src: "https://avatars.githubusercontent.com/u/66021781?v=4", alt: "Headshot of Paul"},
+        links: [
+            {icon: ICONS('github'), href: 'https://github.com/itspaulyg'}
+        ]
     },
     {
         name: "Nolan McShea",
-        title: "Sussy Baka",
-        img: { src: "https://avatars.githubusercontent.com/u/10440773?v=4", alt: ""},
-        links: []
+        title: "Community Manager",
+        img: { src: "https://avatars.githubusercontent.com/u/10440773?v=4", alt: "Headshot of Nolan"},
+        links: [
+            {icon: ICONS('github'), href: 'https://github.com/nuolong'}
+        ]
     }
 ];
